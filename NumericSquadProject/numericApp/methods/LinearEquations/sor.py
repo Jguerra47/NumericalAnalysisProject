e3feae8362046e382e9e9e1702a4f19edeb085e8
Juanda0 <juandavid.echeverri@mercadolibre.com.co>
2022-11-17 15:57:27 -0500

    format seidelsor

--- NumericSquadProject/numericApp/methods/LinearEquations/sor.py
+++ NumericSquadProject/numericApp/methods/LinearEquations/sor.py
@@ -32,10 +32,10 @@ def sor(A, b, tol, w):
 			exit('La matriz no converge')
 	itera = 0
 	err = None
-
+	iterations = []
 	while(norm(minus(Xk1,Xk)) / float(norm(Xk1))) > tol:
 		rowAux = [itera] + ["%.5f"%value for value in Xk] + [err] 
-		table.add_row(rowAux)
+		iterations.append(rowAux)
 		Xk[:] = Xk1[:]
 		for i in range(n):
 			sumation1 = sum(A[i][j]*Xk1[j] for j in range(i))
@@ -44,45 +44,35 @@ def sor(A, b, tol, w):
 			Xk1[i] = (float(w)/A[i][i])*(b[i] - sumation1 - sumation2) + (1-w)*Xk[i]
 		itera += 1
 
-	return Xk1
+	return Xk1,iterations
 
 
 def norm(x):
     return linalg.norm(x) #norm2
 
 
+
 a = [[4,-1,0,3],[1,15.5,3,8],[0,-1.3,-4,1.1],[14,5,-2,30]]
 b=[1,1,1,1]
 tol=1e-7
 w=0.7
-n = len(a)
 
-x = sor(a,b,tol,w)
-table.field_names = ["Iteracion"] + [f"x{i}" for i in range(n)]  + ["Error"]
-print(table)
-
- 
-D = np.diag(np.diag(a))
-L = np.tril(a,-1)
-U = np.triu(a,1)
+def sorAns(a,b,tol,w):
+	x,iter = sor(a,b,tol,w)
+	D = np.diag(np.diag(a))
+	L = np.tril(a,-1)
+	U = np.triu(a,1)
 
 
-Tmatrix = np.dot(linalg.inv(D - (w*L)),((1-w)*D)+(w*U))
+	Tmatrix = np.dot(linalg.inv(D - (w*L)),((1-w)*D)+(w*U))
 
-#print("T Matrix:")
-#prettyPrint("T",Tmatrix)
- 
-valor1 = np.linalg.eig(Tmatrix)
-lista = []
-for i in range(len(valor1)):
-    lista.append(abs(valor1[i]))
-value = max(lista[0])
- 
-#print("The spectral radius is: ")
-#print(value)
- 
-table = PrettyTable()
-table.field_names = [f"x{i}" for i in range(n)]
-table.add_row(x)
-print("\nX:")
-print(table)
+	
+	valor1 = np.linalg.eig(Tmatrix)
+	lista = []
+	for i in range(len(valor1)):
+		lista.append(abs(valor1[i]))
+	spectralRadious = max(lista[0])
+
+	return x,Tmatrix,iter,spectralRadious
+	
+print(sorAns(a,b,tol,w))
