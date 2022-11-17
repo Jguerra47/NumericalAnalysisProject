afc16e5ec03b69b6f622cc01c25a4b00c26d5ba6
jravelon <jacobo.rave@mercadolibre.com.co>
2022-11-17 01:51:15 -0500

    jacobi

--- NumericSquadProject/numericApp/views.py
+++ NumericSquadProject/numericApp/views.py
@@ -4,9 +4,10 @@ from numericApp.methods.Interpolation.lagrange import lagrange
 from numericApp.methods.Interpolation.divideddiff import newton_interpolation
 from numericApp.methods.Interpolation.spline1 import spline1Ans
 from numericApp.methods.Interpolation.spline2 import spline2Ans
-#from numericApp.methods.Interpolation.spline3 import spline3Ans
+from numericApp.methods.Interpolation.spline3 import spline3Ans
 from numericApp.methods.Roots.secant import secant
 from numericApp.methods.LinearEquations.crout import croutAns
+from numericApp.methods.LinearEquations.jacobi import jacobi_Ans
 # Create your views here.
 from math import sqrt
 
@@ -91,12 +92,12 @@ def splines_ep(request):
         
         segments1,polBySeg1 = spline1Ans(X,Y)
         segments2,polBySeg2 = spline2Ans(X,Y)
-        #segments3,polBySeg3 = spline3Ans(X,Y)
+        segments3,polBySeg3 = spline3Ans(X,Y)
         return render(request, "numericApp/splines.html", {
             "state": 1, #Carga correcta
             "segments1": zip(segments1,polBySeg1),
             "segments2": zip(segments2,polBySeg2),
-            #"segments3": zip(segments3,polBySeg3),
+            "segments3": zip(segments3,polBySeg3),
             "X":X,
             "Y":Y,
             "size":size
@@ -154,3 +155,40 @@ def crout_ep(request):
             })
     else:
         return render(request, "numericApp/crout.html")
+
+
+def jacobi_ep(request):
+    if request.method == 'POST':
+        A = []
+        b = [] 
+        x0 = []
+
+        size = int(sqrt(len(request.POST)))-1
+        for i in range(size):
+            b.append([float(request.POST["B"+str(i)])])
+            x0.append([float(request.POST["x0"+str(i)])])
+
+        for i in range(size):
+            q = []
+            for j in range(size):
+                q.append(float(request.POST["A"+str(i)+str(j)]))
+            A.append(q)
+        
+        x,Tmatrix,iterations,spectralRadious = jacobi_Ans(A,b,float(request.POST['tolerance']),x0,int(request.POST['niter']))
+        
+
+        return render(request, "numericApp/jacobi.html", {
+            "state":1,
+            "A":A,
+            "b":b,
+            "x0":x0,
+            "size":size,
+            "tolerance":request.POST['tolerance'],
+            "niter":request.POST['niter'],
+            "Tmatrix":Tmatrix,
+            "iterations":iterations,
+            "spectralRadious":spectralRadious,
+            "x":x
+            })
+    else:
+        return render(request, "numericApp/jacobi.html")
